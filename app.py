62ae83510f12dbea59e8bea96a6e38cf451672ec
sostaocperu <sorizano@taocperu.com>
2024-11-05 22:17:27 -0500

    quitando espacios

--- app.py
+++ app.py
@@ -8,27 +8,27 @@ url = "https://pbsjxezvcmdlwusveukd.supabase.co"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBic2p4ZXp2Y21kbHd1c3ZldWtkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY1MzYxMjgsImV4cCI6MjA0MjExMjEyOH0.LPj5kYWsWQ-wfLry8FBTpXiJxUUVI5qNCTL2CBKMxYY"
 supabase: Client = create_client(url, key)
 
-#Cargar datos de Supabase
+# Cargar datos de Supabase
 def cargar_datos_supabase():
     response = supabase.table("ventas_cafe").select("*").execute()
-
-    #verificar si la respuesta contiene errores
+    
+    # Verificar si la respuesta contiene errores
     if response.error:
-        st.error(f"Error al cargar datos de Supabase: {response.error}")
+        st.error(f"Error al cargar datos desde Supabase: {response.error}")
         return pd.DataFrame()
     else:
         return pd.DataFrame(response.data)
 
-#Datos
+# Datos
 df = cargar_datos_supabase()
 
-#Interfaz de Streamlit
-st.title("Dasboard de Ventas de Café")
-st.subheader("Análisis de ventas de café")
+# Interfaz de Streamlit
+st.title("Dashboard de Ventas de Café")
+st.subheader("Análisis de Ventas de Café")
 
-#Filtro por fecha
-fecha_seleccionada = st.date_input("Selecciona una fecha", value = pd.to_datetime("2024-11-01"))
+# Filtro por fecha
+fecha_seleccionada = st.date_input("Selecciona una fecha", value=pd.to_datetime("2024-11-01"))
 df_filtrado = df[df['fecha'] == fecha_seleccionada]
 
-#Mostrar los datos filtrados
+# Mostrar datos filtrados
 st.write("Datos de ventas para la fecha seleccionada:", df_filtrado)
