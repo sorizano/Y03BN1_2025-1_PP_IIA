f559dbe8a8666a2c8d694d15d7fd77c40bb93ce5
Sergio Orizano Salvador <svorizano@gmail.com>
2024-11-26 22:19:01 -0500

    dos puntos

--- RandomF.py
+++ RandomF.py
@@ -63,7 +63,7 @@ st.write("¿Comprará el producto?", "Sí" if prediction else "No")
 
 
 #11. Botón para guardar predicción
-if st.button("Guardar predicción")
+if st.button("Guardar predicción"):
     result = save_prediction_to_supabase(edad, salario, estado_civil, prefiction)
 
     if result.status_code == 201:
